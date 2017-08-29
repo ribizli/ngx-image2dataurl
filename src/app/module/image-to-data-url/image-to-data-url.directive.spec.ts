5b27c6483663668c101ed8b8cd9f86c905d177f8
ribizli <ribizli@gmail.com>
2017-08-29 16:24:09 +0200

    removed unused packages and testing

--- src/app/module/image-to-data-url/image-to-data-url.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImageToDataUrlDirective } from './image-to-data-url.directive';
-
-describe('ImageToDataUrlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImageToDataUrlDirective();
-    expect(directive).toBeTruthy();
-  });
-});
